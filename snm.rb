0a11d19136f33c11d89078a4522cf76b38a3f5db
ityuany <519495771@qq.com>
2024-06-02 22:58:01 +0800

    add snm

---
+++ snm.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Snm < Formula
+  desc "snm = corepack + fnm + ni ."
+  homepage "https://github.com/sheinsight/snm"
+  url "https://github.com/sheinsight/snm/releases/download/v0.0.1-33/snm-macos-arm.zip"
+  sha256 "40894a71dd76eee97cbbdf002e2c49f17036f035e487657c67da0d312c85761b"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "snm"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test snm`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    # system "false"
+    system "#{bin}/snm", "--version"
+  end
+end
