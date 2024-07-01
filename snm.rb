dadb33f8c87b1af7708606f579cdbd63c4c944d0
ityuany <519495771@qq.com>
2024-07-01 15:39:36 +0800

    update

--- snm.rb
+++ snm.rb
@@ -1,13 +1,13 @@
 class Snm < Formula
   desc "snm = corepack + fnm + ni ."
   homepage "https://github.com/sheinsight/snm"
-  version "v0.0.1-83"
+  version "v0.0.1-91"
   if Hardware::CPU.intel?
-    url "https://github.com/sheinsight/snm/releases/download/v0.0.1-83/x86_64-apple-darwin.tar.gz"
-    sha256 "8ce62abe2b96f16dfd8d956cf47360745e0c48fd6598bf46e84c05f5fb2dc522"
+    url "https://github.com/sheinsight/snm/releases/download/v0.0.1-91/x86_64-apple-darwin.tar.gz"
+    sha256 "cee90bab56966246f41fdd948bda1237df55dbee8495821ee96d940674f39020"
   elsif Hardware::CPU.arm?
-    url "https://github.com/sheinsight/snm/releases/download/v0.0.1-83/aarch64-apple-darwin.tar.gz"
-    sha256 "e92a3ae6bd648f4db75755b2c0347342d5cf893b62dfa6b9462f2dfc27cf5d59"
+    url "https://github.com/sheinsight/snm/releases/download/v0.0.1-91/aarch64-apple-darwin.tar.gz"
+    sha256 "10d4bcd8ce1eba8a8ac38e33c05b20bb756303df52ba93fa6623260819640e09"
   end
   license "MIT"
   def install
