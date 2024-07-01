0ad1f1f63d42a12bdc618078205e37d2f3548a01
ityuany <519495771@qq.com>
2024-07-01 11:17:08 +0000

    snm 0.0.1-93

--- snm.rb
+++ snm.rb
@@ -1,10 +1,10 @@
 class Snm < Formula
   desc "snm = corepack + fnm + ni ."
   homepage "https://github.com/sheinsight/snm"
-  version "v0.0.1-91"
+  version "0.0.1-93"
   if Hardware::CPU.intel?
-    url "https://github.com/sheinsight/snm/releases/download/v0.0.1-91/x86_64-apple-darwin.tar.gz"
-    sha256 "cee90bab56966246f41fdd948bda1237df55dbee8495821ee96d940674f39020"
+    url "https://github.com/sheinsight/snm/releases/download/0.0.1-93/x86_64-apple-darwin.tar.gz"
+    sha256 "1125060130208468b5a52127733d3bff5c541a5bf022c6119d6d51730a30aa46"
   elsif Hardware::CPU.arm?
     url "https://github.com/sheinsight/snm/releases/download/v0.0.1-91/aarch64-apple-darwin.tar.gz"
     sha256 "10d4bcd8ce1eba8a8ac38e33c05b20bb756303df52ba93fa6623260819640e09"
