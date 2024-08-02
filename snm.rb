0b6fa1ffc2c2dec7fdc1a562eaf649baf14be349
ityuany <ityuany@users.noreply.github.com>
2024-08-02 06:54:12 +0000

    Auto-generated PR from repository A

--- snm.rb
+++ snm.rb
@@ -1,13 +1,13 @@
 class Snm < Formula
   desc "snm = corepack + fnm + ni ."
   homepage "https://github.com/sheinsight/snm"
-  version "0.0.1-113"
+  version "0.0.1-114"
   if Hardware::CPU.intel?
-    url "https://github.com/sheinsight/snm/releases/download/0.0.1-113/x86_64-apple-darwin.tar.gz"
-    sha256 "814c83b15c9134e3cb61625ef1a79052379b694274bc9f9207f871eaf9749340"
+    url "https://github.com/sheinsight/snm/releases/download/0.0.1-114/x86_64-apple-darwin.tar.gz"
+    sha256 "8c25a782a48cfc05440a67e5eafec8066dea626db849ddd7ede67f9c8b88255c"
   elsif Hardware::CPU.arm?
-    url "https://github.com/sheinsight/snm/releases/download/0.0.1-113/aarch64-apple-darwin.tar.gz"
-    sha256 "8d95efd27f2fcf212f4d9c6d8e385db740266b24733896e0e301d953e2a1a5e8"
+    url "https://github.com/sheinsight/snm/releases/download/0.0.1-114/aarch64-apple-darwin.tar.gz"
+    sha256 "e1a7701180bc3ab12ddcdcd5182da0fc371c9ec02da13eca40281e559a854438"
   end
   license "MIT"
   def install
